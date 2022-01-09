6ce4d7d38deafd6cdd6bf5e462402fb3a1ae87a4
Jackbaklava <mhqadir14@gmail.com>
2022-01-09 13:26:12 +0500

    Added static files and users can no longer choose the host when creating a room.

--- base/views.py
+++ base/views.py
@@ -128,7 +128,10 @@ def create_room(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
+            room.participants.add(request.user)
             return redirect("home")
 
     context = {"form": form}
